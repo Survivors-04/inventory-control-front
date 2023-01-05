8cbf00fbc44ba2e75f604bbe1dd4e271cb1ff33d
Enrique <enrique.barbosasilva@gmail.com>
2023-01-05 15:57:54 -0300

    feat: page-dashboard-manager-created

---
+++ src/pages/Manager/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+//import { StyledMain, StyledUl } from "./style";
+
+const ManagerViewPage = () => {
+  return (
+    <>
+      <Header />
+
+      <Main />
+    </>
+  );
+};
+
+export default ManagerViewPage;
