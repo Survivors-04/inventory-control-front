8bc499519e0179d7fe3221ebf4a991d078717e32
Edmar Cardoso <ed2019junior@gmail.com>
2023-01-05 22:47:03 -0300

    feat: add-hiperlinks-in-header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,55 @@
-import { StyledHeader } from "./style";
+import { ContainerHeader, StyledHeader, ModalHeaderStyled } from "./style";
 import logoHeader from "../../../src/assets/imgs/Inventory control.png";
 import logoCaixaHeader from "../../../src/assets/imgs/caixa.png";
 import { useState } from "react";
 import Button from "../Button";
+import ModalBase from "../ModalBase";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
-  const [isLogged] = useState(false);
+  const [isLogged] = useState(true);
+  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
-    <StyledHeader>
-      <div>
-        <img src={logoHeader} alt="imageInventoryControl" />
-        <img src={logoCaixaHeader} alt="boxImage" className="test" />
-      </div>
+    <ContainerHeader>
+      <StyledHeader
+        logged={isLogged}
+        isOpenModal={isOpenModal}
+        className="test"
+      >
+        <div>
+          <img src={logoHeader} alt="imageInventoryControl" />
+          <img src={logoCaixaHeader} alt="boxImage" />
+        </div>
+        {isOpenModal ? (
+          <ModalBase
+            setIs={setIsOpenModal}
+            heigth="500px"
+            width="250px"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <ModalHeaderStyled>
+              <Link to="/">Produtos</Link>
+              <Link to="/dashboard-account">Pedidos</Link>
+              <Button>Sair</Button>
+            </ModalHeaderStyled>
+          </ModalBase>
+        ) : (
+          <button onClick={() => setIsOpenModal(true)}>
+            <GiHamburgerMenu />
+          </button>
+        )}
 
-      {isLogged ? <Button>Sair</Button> : null}
-    </StyledHeader>
+        {isLogged ? (
+          <nav>
+            <Link to="/">Produtos</Link>
+            <Link to="/dashboard-account">Pedidos</Link>
+            <Button>Sair</Button>
+          </nav>
+        ) : null}
+      </StyledHeader>
+    </ContainerHeader>
   );
 };
 
---
+++ src/pages/DashboardAccount/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./style";
+import Header from "../../components/Header";
+
+const DashboardAccount = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default DashboardAccount;
