bd2e18ab30be8b20f9fe609770ba03f4ebfd9c34
Enrique <enrique.barbosasilva@gmail.com>
2023-01-06 02:24:27 -0300

    feat: productForm.validations-created

---
+++ src/validations/productForm.validations.ts
@@ -0,0 +1,8 @@
+import * as yup from "yup";
+
+export const productFormSchema = yup.object().shape({
+  name: yup.string().required("Nome obrigatório!"),
+  description: yup.string().required("Descrição obrigatória!"),
+  price: yup.string().required("Preço obrigatório!"),
+  amount: yup.string().required("Preço obrigatório!"),
+});
