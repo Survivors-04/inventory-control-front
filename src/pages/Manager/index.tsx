07d1e6a3194edc7c1796c72db7dd455b8ce4a080
Enrique <enrique.barbosasilva@gmail.com>
2023-01-06 02:23:59 -0300

    refactor: modal-form-added

--- src/pages/Manager/index.tsx
+++ src/pages/Manager/index.tsx
@@ -1,13 +1,12 @@
 import Header from "../../components/Header";
 import Main from "../../components/Main";
-//import { StyledMain, StyledUl } from "./style";
 
 const ManagerViewPage = () => {
   return (
     <>
       <Header />
 
-      <Main />
+      <Main formSubmit={() => {}} />
     </>
   );
 };
