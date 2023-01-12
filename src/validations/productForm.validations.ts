b313d8066b8720c432cf2e5f821007f0c3ba3392
Enrique <enrique.barbosasilva@gmail.com>
2023-01-12 16:18:16 -0300

    refactor: productForm-validations

--- src/validations/productForm.validations.ts
+++ src/validations/productForm.validations.ts
@@ -3,6 +3,6 @@ import * as yup from "yup";
 export const productFormSchema = yup.object().shape({
   name: yup.string().required("Nome obrigatório!"),
   description: yup.string().required("Descrição obrigatória!"),
-  price: yup.string().required("Preço obrigatório!"),
+  price: yup.number().required("Preço obrigatório!"),
   amount: yup.number().required("Montante obrigatório!"),
 });
